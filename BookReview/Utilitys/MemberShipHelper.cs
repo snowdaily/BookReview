7080b8988063fbf393b1f35f08c0cbcb601fd91f
Gary <snow.daily@gamil.com>
2013-02-05 21:45:48 +0800

    Test

---
+++ BookReview/Utilitys/MemberShipHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.WebData;
+
+namespace BookReview.Utilitys
+{
+    public class MemberShipHelper
+    {
+        //public override void WebSecurity()
+        //{
+            
+        //}
+    }
+}
